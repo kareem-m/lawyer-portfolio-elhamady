b66664cb414f1a05b6dc83907256b4cd40cbdc5a
kareem <kareemmoh1911@gmail.com>
2026-07-02 15:32:55 +0300

    make ar is the default language

--- src/middleware.js
+++ src/middleware.js
@@ -3,7 +3,8 @@ import createMiddleware from 'next-intl/middleware';
 export default createMiddleware({
     locales: ['ar', 'en'],
     defaultLocale: 'ar',
-    localePrefix: 'as-needed'
+    localePrefix: 'as-needed',
+    localeDetection: false
 });
 
 export const config = {
